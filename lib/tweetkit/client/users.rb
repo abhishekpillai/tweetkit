e203eb8485c3ac5770bec41dfc4ea6a3e12df862
Abhi Pillai <abhi.pillai1@gmail.com>
2022-02-12 00:15:50 -0500

    Trying to fix issue with with including module

--- lib/tweetkit/client/users.rb
+++ lib/tweetkit/client/users.rb
@@ -1,13 +1,11 @@
 module Tweetkit
-    class Client
-        module Users
-            def following(id, **options)
-                get "users/#{id}/following", **options
-            end
-        
-            def followers(id, **options)
-                get "users/#{id}/followers", **options
-            end
+    module Client::Users
+        def following(id, **options)
+            get "users/#{id}/following", **options
+        end
+
+        def followers(id, **options)
+            get "users/#{id}/followers", **options
         end
     end
 end
